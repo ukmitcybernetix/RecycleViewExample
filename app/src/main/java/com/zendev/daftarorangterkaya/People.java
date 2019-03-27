b37587225b49c92132e4d4ba0f3acf1b61dcb837
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-03-27 23:17:38 +0700

    Menambahkan Kelas POJO

---
+++ app/src/main/java/com/zendev/daftarorangterkaya/People.java
@@ -0,0 +1,38 @@
+package com.zendev.daftarorangterkaya;
+
+public class People {
+
+    String name, founder, deskripsi, photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFounder() {
+        return founder;
+    }
+
+    public void setFounder(String founder) {
+        this.founder = founder;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
